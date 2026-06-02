919fddf7fb84e29f3d067b5f2722b35d21591981
Siarhei <sergeybednyuk@gmail.com>
2026-06-02 22:36:36 +0200

    feat[chili-flow.artist] add popilar artist on main page and atrist page UI (#20)

--- src/app/services/jamendo-api.types.ts
+++ src/app/services/jamendo-api.types.ts
@@ -44,3 +44,63 @@ export interface GetArtistTracksApiResponse {
   headers: JamendoHeadersApiResponse;
   results: ArtistTracksApiResponse[];
 }
+
+
+export interface GetFullArtistInfoApiResponse {
+  headers: JamendoHeadersApiResponse;
+  results: ArtistApiResponse[];
+}
+
+
+export interface GetArtistAlbumsApiResponse {
+  headers: JamendoHeadersApiResponse;
+  results: ArtistAlbumsApiResponse[];
+}
+
+export interface ArtistAlbumsApiResponse {
+  id: string;
+  name: string;
+  releasedate: string;
+  artist_id: string;
+  artist_name: string;
+  image: string;
+  zip: string;
+  shorturl: string;
+  shareurl: string;
+  zip_allowed: boolean;
+}
+
+export interface GetArtistTracksApiResponse {
+  headers: JamendoHeadersApiResponse;
+  results: ArtistTracksApiResponse[];
+}
+
+
+export interface GetFullArtistInfoApiResponse {
+  headers: JamendoHeadersApiResponse;
+  results: ArtistApiResponse[];
+}
+
+
+export interface GetArtistAlbumsApiResponse {
+  headers: JamendoHeadersApiResponse;
+  results: ArtistAlbumsApiResponse[];
+}
+
+export interface ArtistAlbumsApiResponse {
+  id: string;
+  name: string;
+  releasedate: string;
+  artist_id: string;
+  artist_name: string;
+  image: string;
+  zip: string;
+  shorturl: string;
+  shareurl: string;
+  zip_allowed: boolean;
+}
+
+export interface GetArtistTracksApiResponse {
+  headers: JamendoHeadersApiResponse;
+  results: ArtistTracksApiResponse[];
+}
